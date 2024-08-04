42436205dec4da45323aee00b40f4d2a65894a44
JoshuyPan <leonardjsparks@gmail.com>
2024-08-04 22:13:34 +0200

    first commit

---
+++ test/unit/DeployMoodNftTest.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
+import {MoodNft} from "src/MoodNft.sol";
+import {DeployMoodNft} from "script/DeployMoodNft.s.sol";
+
+contract DeployMoodNftTest is Test {
+    DeployMoodNft public deployer;
+
+    function setUp() public {
+        deployer = new DeployMoodNft();
+    }
+
+    function testConvertSvgToUri() public view {
+        string
+            memory expectedOutput = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHhtbG5zOnhsaW5rPSdodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rJyB3aWR0aD0nNTAwJyBoZWlnaHQ9JzUwMCc+PHRleHQgeD0nMCcgeT0nMTUnIGZpbGw9J2JsYWNrJz5IaSEgWW91ciBicm93c2VyIGRlY29kZWQgdGhpczwvdGV4dD48L3N2Zz4=";
+        string
+            memory svg = "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='500' height='500'><text x='0' y='15' fill='black'>Hi! Your browser decoded this</text></svg>";
+        string memory actualUri = deployer.svgToImageURI(svg);
+        assert(
+            keccak256(abi.encodePacked(actualUri)) ==
+                keccak256(abi.encodePacked(expectedOutput))
+        );
+    }
+}
